2f94cb84f043268c1ce3c230dd415a58c01249a3
wentzel16 <43389665+wentzel16@users.noreply.github.com>
2018-09-20 23:15:36 +0000

    First draft of the code
    First draft of the code

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,12 +1,42 @@
 #Tile Traveller board game first half
 
-Direction=input(str("Insert a direction N, S, E or W: ")).lower()
+#Skilgreini x og y. x stendur fyrir fyrri tölu og y fyrir seinni tölu
+
+while True:
+
+    direction = (input("input a direction: "))
+
+    x = 1
+    y = 1
+
+    
 
 
-if Direction == "n":
-    print("you can travel (n)orth or (e)ast or (s)outh")
-else: print("not a valid direction!")
+#North
+    if direction == "N".lower() and direction < 3:
+        y+=1
+        print(x,y)
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+#South
+    if direction == "S".lower() and direction > 1:
+        print(x,y)
+        y-=1
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+
+#east
+    if direction == "E".lower() and direction < 3:
+        x+=1
+        print("You can travel: (N)orth or (E)ast or (S)outh.") 
+
+#west
+    if direction == "W".lower() and direction > 1:
+        x-=1
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
 
 
 
-# Just trying out my git
+
+
+
+
+
